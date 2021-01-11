964b092d433b47f46f90ea7f61590cde7ae38e15
Tuc Baldecchi <tucbaldecchi@tucs-mbp.myfiosgateway.com>
2021-01-11 15:32:54 -0500

    added notes for recursion and created recursion function

---
+++ Recursion/ExampleFunctions/factorial.js
@@ -0,0 +1,18 @@
+// function factorial(num) {
+//   let total = 1;
+//   for (let i = num; i > 1; i--) {
+//     total *= i;
+//   }
+//   return total;
+// }
+
+//Turn this function into a recursive one
+
+function factorial(num) {
+  if (num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(4));
--- Recursion/ExampleFunctions/sumRange.js
+++ Recursion/ExampleFunctions/sumRange.js
@@ -3,8 +3,18 @@ function sumRange(num) {
   return num + sumRange(num - 1);
 }
 
-sumRange(1);
+console.log(sumRange(4));
 
 //base case when num === 0
 //input is the number that is being decreased by 1 over time
-//if we didn't return
+//if we didn't return then the function would run forever
+
+// 4 + sumRange(3)
+//3 + sumRange(2)
+//2 + sumRange(1)
+//1
+
+// 4 + 6
+//3 + 3
+//2 + 1
+//1
