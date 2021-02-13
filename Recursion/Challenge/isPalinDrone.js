e7a52c324b04ade95eca02797f7ac3941772feae
Tuc Baldecchi <tucbaldecchi@tucs-mbp.myfiosgateway.com>
2021-02-13 16:54:45 -0500

    finshed isPalinDrone algothrium

--- Recursion/Challenge/isPalinDrone.js
+++ Recursion/Challenge/isPalinDrone.js
@@ -1,16 +1,24 @@
-function isPalindrome(string) {
-  let length = string.length;
-  let result = isPalindrome(string.slice(1)) + string[0];
-  if (length === 1) {
-    return string;
-  } else if (string === result) {
-    return true;
-  }
-  return false;
-}
-
 // isPalindrome('awesome') // false
 // isPalindrome('foobar') // false
 // isPalindrome('tacocat') // true
 // isPalindrome('amanaplanacanalpanama') // true
 // isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(string) {
+  if (string.length <= 1) {
+    return true;
+  }
+
+  let [firstLetter] = string;
+  let lastLetter = string[string.length - 1];
+
+  if (firstLetter === lastLetter) {
+    let stringWithoutFirstAndLastLetters = string.substring(
+      1,
+      string.length - 1
+    );
+    return isPalindrome(stringWithoutFirstAndLastLetters);
+  } else {
+    return false;
+  }
+}
