f5fde960d652739b4bd34138e00245073c35f582
Tuc Baldecchi <tucbaldecchi@Tucs-MacBook-Pro.local>
2021-04-14 18:24:45 -0400

    finshed naive string search

---
+++ Search Algothrium/problemSet/naiveString.js
@@ -0,0 +1,20 @@
+function naiveSearch(array, string) {
+  let first = string[0];
+  let len = string.length;
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === first) {
+      for (let j = 0; j < len; j++) {
+        if (array[i + j] !== string[j]) {
+          j = len;
+        }
+        if (j === len - 1) {
+          count += 1;
+        }
+      }
+    }
+  }
+  return count;
+}
+
+console.log(naiveSearch("lorie  loled", "e"));
