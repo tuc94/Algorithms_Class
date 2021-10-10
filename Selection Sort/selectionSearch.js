da7f44ebc02be9473383ecad4d9dfc7311a671f3
Tuc Baldecchi <tucbaldecchi@tucs-mbp.myfiosgateway.com>
2021-10-10 17:30:26 -0400

    Added the selection sort section

--- Selection Sort/selectionSearch.js
+++ Selection Sort/selectionSearch.js
@@ -1,47 +1,17 @@
 function selectionSort(array) {
-  let swaps;
-  //create a loop that stops when all the elements are in the correct order
-  for (let i = array.length; i !== 0; i--) {
-    swaps = false;
-    let min;
-    for (let j = 0; j < array.length; j++) {
-      if (array[j] < array[0]) {
-        min = array[j];
-        swaps = true;
+  for (let i = 0; i < array.length; i++) {
+    let lowest = i;
+    for (j = i + 1; j < array.length; j++) {
+      if (array[j] < array[lowest]) {
+        console.log(j);
+        lowest = j;
       }
     }
-    if (swaps) {
-      let changeElement = array.indexOf(min);
-      let orginal = array[0];
-      console.log(changeElement, orginal);
-      array[0] = min;
-      array[changeElement] = orginal;
-    }
-    if (!swaps) break;
+    let temp = array[i];
+    array[i] = array[lowest];
+    array[lowest] = temp;
   }
   return array;
 }
-
-// function selectionSort(array) {
-//   //Create a loop that looks at all the elements
-//   let swap;
-
-//   let min = array[0];
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] < min) {
-//       min = array[i];
-//       swap = true
-//     }
-//   }
-//   //element of min
-//   console.log(min)
-//   let changeElement = array.indexOf(min);
-//   let orginal = array[0];
-//   console.log(changeElement, orginal)
-//   array[0] = min;
-//   array[changeElement] = orginal;
-//   return array;
-// }
-
 //5,3,4,1,2
-console.log(selectionSort([34, 22, 10, 19, 17]));
+console.log(selectionSort([17, 43, 22, 50]));
