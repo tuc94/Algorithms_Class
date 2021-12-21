9b1fd7e4e41623ca388e863263f33a8660f66b4c
Tuc Baldecchi <tucbaldecchi@tucs-mbp.myfiosgateway.com>
2021-12-21 17:37:23 -0500

    Added shift and unshift methods

---
+++ Single Linked Lists/unshift.js
@@ -0,0 +1,72 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    var currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val) {
+    var newNode = new Node(val);
+    var currentHead = this.head;
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.head = newNode;
+      this.head.next = currentHead;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+var first = new SinglyLinkedList();
+first.push("Pincles");
+first.push("Basil");
+first.push("Basil2");
+console.log(first.unshift("Tuc"));
