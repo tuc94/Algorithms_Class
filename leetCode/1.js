9b1f6753da9e0bdfc51a3acdba4146b71096643c
Tuc Baldecchi <tucbaldecchi@tucs-mbp.myfiosgateway.com>
2022-02-03 17:39:08 -0500

    attempted to get this problem soloved

--- leetCode/1.js
+++ leetCode/1.js
@@ -9,29 +9,36 @@ var twoSum = function(array, target) {
     let endstart = 0;
     //output array
     let output = [];
+    //count varaible 
+    let count = 1;
     //loop through the array 
     for(let i = 0; i < array.length; i++){
-        sum = 0 
-        sum += array[i]
         //inner loop through all other stuff and checks if it works
-        if(!skip){
-        for(let j = i+1; j < array.length; j++){
+        while(!skip && count !== array.length + 1){
+        sum = array[i]
+        //need a loop that increments after each loop
+        for(let j = i + count; j < array.length; j++){
             sum += array[j]
             if(target < sum){
                 j = array.length;
-            } else if(sum ===target){
+            } else if(sum === target){
                 endstart =i;
                 end = j;
-            }
+                count = array.length
+                skip = true
+            } 
         }
+        count += 1;
         }
+        count = 0
     }
     output = [endstart,end];
     return output
 };
 
-console.log(twoSum([3,2,3],6)
-
+//i=0, j=1
+//loop
+//i=0, j=2
 
 // var twoSum = function(array, target) {
 //     //create a varaible to capture sums
